c3616eb415fb04624772a9b4d55a681e3357ca97
Jesús Alberto Carballo Ruelas <chuuy8550@gmail.com>
2026-03-04 10:32:29 -0600

    feat(extraccion): Ahora puede procesar tanto archivos puntuales de una carpeta como la carpeta completa

--- extraccion-v-100/extraccion.py
+++ extraccion-v-100/extraccion.py
@@ -10,38 +10,59 @@ def procesar():
 
     data = request.get_json()
     ruta = data.get("ruta")
+    archivos = data.get("archivos")
 
     if not ruta:
         return jsonify({"error": "No se recibió ruta"}), 400
 
     if not os.path.exists(ruta):
         return jsonify({"error": "La ruta no existe"}), 400
+    
+    resultados = []
 
     try:
-        # -------- EXTRAER TEXTO --------
-        reader = PdfReader(ruta)
+        # -------- UNIR RUTA Y ARCHIVO --------
+        for nombre_archivo in archivos:
 
-        texto = ""
-        for pagina in reader.pages:
-            contenido = pagina.extract_text()
-            if contenido:
-                texto += contenido + "\n"
+            ruta_completa = os.path.join(ruta, nombre_archivo)
+
+            if not os.path.exists(ruta_completa):
+                resultados.append({
+                    "archivo": nombre_archivo,
+                    "error": "No existe"
+                })
+                continue
+
+        # -------- EXTRAER TEXTO --------
+            reader = PdfReader(ruta_completa)
+
+            texto = ""
+            for pagina in reader.pages:
+                contenido = pagina.extract_text()
+                if contenido:
+                    texto += contenido + "\n"
 
         # -------- CREAR PAYLOAD PARA KB --------
-        payload = {
-            "archivo": os.path.basename(ruta),
-            "contenido": texto
-        }
+            payload = {
+                "archivo": nombre_archivo,
+                "contenido": texto
+            }
 
         # -------- ENVIAR A KB --------
-        respuesta = requests.post(
-            'http://10.9.26.92:3000/extraccion',
-            json=payload
-        )
+            respuesta = requests.post(
+                'http://10.9.26.92:3000/extraccion',
+                json=payload
+            )
 
+            resultados.append({
+                "archivo": nombre_archivo,
+                "status_envio": respuesta.status_code
+            })
+
+        # ------- RESPUESTA FINAL A INTERFAZ --------
         return jsonify({
-            "mensaje": "Archivo procesado y enviado",
-            "respuesta_servidor_final": respuesta.json()
+            "mensaje": "Archivos procesados y enviados",
+            "resultados": resultados
         })
 
     except Exception as e:
